7d343b46021c4ad44c831ad8ae720dbba0272fe9
odsliu <oscarliu2013@tutamail.com>
2025-11-22 08:33:48 +0800

    tesser.conf

--- src/common/args.cpp
+++ src/common/args.cpp
@@ -34,7 +34,7 @@
 #include <utility>
 #include <variant>
 
-const char * const BITCOIN_CONF_FILENAME = "bitcoin.conf";
+const char * const BITCOIN_CONF_FILENAME = "tesser.conf";
 const char * const BITCOIN_SETTINGS_FILENAME = "settings.json";
 
 ArgsManager gArgs;
