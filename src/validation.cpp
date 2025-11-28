740225966058ee37259c48acbae5f86a11f0b211
odsliu <oscarliu2013@tutamail.com>
2025-11-28 20:03:16 +0800

    modify CMakeLists.txt stuff fix name issues

--- src/validation.cpp
+++ src/validation.cpp
@@ -1994,7 +1994,7 @@ void Chainstate::InitCoinsCache(size_t cache_size_bytes)
 //
 bool ChainstateManager::IsInitialBlockDownload() const
 {
-    //ibd
+    // ibd
     // Optimization: pre-test latch before taking the lock.
     if (m_cached_finished_ibd.load(std::memory_order_relaxed))
         return false;
