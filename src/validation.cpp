0f5a5c0244e0d7dd137926c95fc1e5849ab64406
odsliu <oscarliu2013@tutamail.com>
2025-12-08 16:38:35 -0500

    ibd

--- src/validation.cpp
+++ src/validation.cpp
@@ -1993,25 +1993,25 @@ bool ChainstateManager::IsInitialBlockDownload() const
 {
     // ibd
     // Optimization: pre-test latch before taking the lock.
-    // if (m_cached_finished_ibd.load(std::memory_order_relaxed))
-    //     return false;
-    //
-    // LOCK(cs_main);
-    // if (m_cached_finished_ibd.load(std::memory_order_relaxed))
-    //     return false;
-    // if (m_blockman.LoadingBlocks()) {
-    //     return true;
-    // }
-    // CChain& chain{ActiveChain()};
-    // if (chain.Tip() == nullptr) {
-    //     return true;
-    // }
-    // if (chain.Tip()->nChainWork < MinimumChainWork()) {
-    //     return true;
-    // }
-    // if (chain.Tip()->Time() < Now<NodeSeconds>() - m_options.max_tip_age) {
-    //     return true;
-    // }
+    if (m_cached_finished_ibd.load(std::memory_order_relaxed))
+        return false;
+
+    LOCK(cs_main);
+    if (m_cached_finished_ibd.load(std::memory_order_relaxed))
+        return false;
+    if (m_blockman.LoadingBlocks()) {
+        return true;
+    }
+    CChain& chain{ActiveChain()};
+    if (chain.Tip() == nullptr) {
+        return true;
+    }
+    if (chain.Tip()->nChainWork < MinimumChainWork()) {
+        return true;
+    }
+    if (chain.Tip()->Time() < Now<NodeSeconds>() - m_options.max_tip_age) {
+        return true;
+    }
     LogInfo("Leaving InitialBlockDownload (latching to false)");
     m_cached_finished_ibd.store(true, std::memory_order_relaxed);
     return false;
